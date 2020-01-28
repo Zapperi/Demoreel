43633d40d0a97288f7516bceb4e62c8cd26be0cf
Henri Leppänen <henri.leppanen@metropolia.fi>
2020-01-28 17:11:55 +0200

    Scene Transistions started
    Weird bug on CrossFade, find it and fix it later

---
+++ Demoreel/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+    public Animator sceneLoaderAnimator;
+    [SerializeField]
+    private float m_transistionTime;
+    private int transistionCount = 2;
+
+    public void Start()
+    {
+            if (!Instance)
+            {
+                Instance = this;
+            }
+            else
+            {
+                DestroyImmediate(gameObject);
+            }
+
+
+        sceneLoaderAnimator.SetTrigger("StartScene1");
+    }
+
+    public void LoadTestScene()
+    {
+        StartCoroutine(LoadScene("TestScene_1"));
+    }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        //int transistionNumer = Random.Range(1, transistionCount);
+        int transistionNumer = 2;
+        sceneLoaderAnimator.SetTrigger("EndScene"+ transistionNumer);
+        yield return new WaitForSeconds(m_transistionTime);
+        MainMenuController.Instance.HideMainMenu();
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        sceneLoaderAnimator.SetTrigger("StartScene"+ transistionNumer);
+    } 
+}

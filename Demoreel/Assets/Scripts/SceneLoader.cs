fa01853694b31991c6b8d5c6ccf661ee81e1f9ab
Henri Leppänen <henri.leppanen@metropolia.fi>
2020-02-01 19:32:24 +0200

    Forgotten Code commenting and clean up

--- Demoreel/Assets/Scripts/SceneLoader.cs
+++ Demoreel/Assets/Scripts/SceneLoader.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Handles scene loading and closing. Plays scene transistion animation between changes.
+/// </summary>
 public class SceneLoader : MonoBehaviour
 {
+    /// <summary>
+    /// Singleton of the SceneLoader class, use this is access the class.
+    /// </summary>
     public static SceneLoader Instance;
+    /// <summary>
+    /// Reference to the scene transistion animation animator.
+    /// </summary>
+    [Tooltip("Reference to the scene transistion animation animator.")]
     public Animator sceneLoaderAnimator;
-    public int transistionCount = 3;
-    public bool forceMode;
-    public int forcedTransistion = 1;
-
+    /// <summary>
+    /// How many transistions there are in total.
+    /// </summary>
+    [Tooltip("How many transistions there are in total.")]
+    public int transistionCount = 3; // Needs to be reworked to be dynamic.
+    /// <summary>
+    /// Forces the scene transistion animator to play a spesific animation.
+    /// </summary>
     [SerializeField]
-    private float m_transistionTime;
+    [Tooltip("Forces the scene transistion animator to play a spesific animation.")]
+    private bool _forceMode = false;
+    /// <summary>
+    /// Which animation is forced.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("Which animation is forced.")]
+    private int _forcedTransistion = 1; // Needs to be reworked into enum.
+
+    /// <summary>
+    /// How long transistion animation holds black screen.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("How long transistion animation holds black screen.")]
+    private float _transistionTime = 2;
 
 
     public void Start()
@@ -28,49 +56,88 @@ public class SceneLoader : MonoBehaviour
             }
     }
 
-    public void LoadTestScene()
+#region ---PUBLIC FUNCTIONS---
+
+    /// <summary>
+    /// Loads spesific TestScene with index. "TestScene_" + number.
+    /// </summary>
+    /// <param name="testSceneIndex">Index of the test scene to load.</param>
+    public void LoadTestScene(int testSceneIndex)
     {
-        StartCoroutine(LoadScene("TestScene_1"));
+        if(testSceneIndex < 1)
+        {
+            Debug.LogWarning("TRYING TO LOAD TESTSCENE INDEX UNDER 1!");
+        }
+        StartCoroutine(LoadScene("TestScene_"+testSceneIndex));
     }
 
+    /// <summary>
+    /// Unloads spesific active scene by given build index.
+    /// </summary>
+    /// <param name="buildIndex">Build index of the scene to unload.</param>
     public void UnloadSceneByIndex(int buildIndex)
     {
         StartCoroutine(UnloadScene(buildIndex));
     }
 
+    #endregion
+    #region ---PRIVATE FUNCTIONS---
+
+    /// <summary>
+    /// Loads a scene by name. Activates random scene transistion animation in between.
+    /// </summary>
+    /// <param name="sceneName">Scene to load.</param>
+    /// <returns></returns>
     private IEnumerator LoadScene(string sceneName)
     {
         int transistionNumber;
-        if (forceMode)
+        // If forced animation mode is activated, uses given animation. Otherwise randomize the animation. 
+        if (_forceMode)
         {
-            transistionNumber = forcedTransistion;
+            transistionNumber = _forcedTransistion;
         }
         else
         {
             transistionNumber = Random.Range(1, transistionCount);
         }
-        sceneLoaderAnimator.SetTrigger("EndScene"+ transistionNumber);
-        yield return new WaitForSeconds(m_transistionTime);
+        // Activate transistion "ToBlack" animation.
+        sceneLoaderAnimator.SetTrigger("EndScene" + transistionNumber);
+        yield return new WaitForSeconds(_transistionTime/2);
+        // While screen is black, hide Main Menu.
         MainMenuController.Instance.HideMainMenu();
+        // Load a new scene while screen is black.
         SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
-        sceneLoaderAnimator.SetTrigger("StartScene"+ transistionNumber);
+        yield return new WaitForSeconds(_transistionTime / 2);
+        // Fade back to game.
+        sceneLoaderAnimator.SetTrigger("StartScene" + transistionNumber);
     }
 
+    /// <summary>
+    /// Unloads spesific scene by it's build index. Activates transistion animation in between.
+    /// </summary>
+    /// <param name="buildIndex">Build index of the scene to unload.</param>
+    /// <returns></returns>
     private IEnumerator UnloadScene(int buildIndex)
     {
         int transistionNumber;
-        if (forceMode)
+        // If forced animation mode is activated, uses given animation. Otherwise randomize the animation. 
+        if (_forceMode)
         {
-            transistionNumber = forcedTransistion;
+            transistionNumber = _forcedTransistion;
         }
         else
         {
             transistionNumber = Random.Range(1, transistionCount);
         }
+        // Activate transistion "ToBlack" animation.
         sceneLoaderAnimator.SetTrigger("EndScene" + transistionNumber);
-        yield return new WaitForSeconds(m_transistionTime);
+        yield return new WaitForSeconds(_transistionTime / 2);
+        // While screen is black, show Main Menu.
         MainMenuController.Instance.ShowMainMenu();
         SceneManager.UnloadSceneAsync(buildIndex);
+        yield return new WaitForSeconds(_transistionTime / 2);
+        // Fade back to game.
         sceneLoaderAnimator.SetTrigger("StartScene" + transistionNumber);
     }
+    #endregion
 }
--- Demoreel/Assets/Scripts/TestScene1BackButton.cs
+++ Demoreel/Assets/Scripts/TestScene1BackButton.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// TestScene1 Button script to return to Main Menu.
+/// </summary>
 public class TestScene1BackButton : MonoBehaviour
 {
     public void BackToMainMenu()
